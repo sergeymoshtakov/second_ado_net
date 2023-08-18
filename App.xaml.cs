ad75239598bbddbda19fd7514c0ac7fec7947c03
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-08-18 21:22:51 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace second
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public const String ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Startklar\\Desktop\\second\\second\\second\\Database1.mdf;Integrated Security=True";
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace second
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection connection;
+        public ObservableCollection<String> columns { get; set; } = new ObservableCollection<String>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            connection = null;
+            this.DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                connection = new SqlConnection(App.ConnectionString);
+                connection.Open();
+                loadGroups();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            connection?.Dispose();
+        }
+
+        private void CreateGroup_Click(object sender, RoutedEventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = @"
+                CREATE TABLE ProductGroups (
+                    Id            UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+                    Name        NVARCHAR(50)     NOT NULL,
+                    Description NTEXT            NOT NULL,
+                    Picture     NVARCHAR(50)     NULL
+                ) ;";
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Table created");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Create Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void InsertGroup_Click(object sender, RoutedEventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = @"
+                INSERT INTO ProductGroups
+                    ( Id, Name,    Description, Picture )
+                VALUES
+                ( '089015F4-31B5-4F2B-BA05-A813B5419285', N'Інструменти',     N'Ручний інструмент для побутового використання', N'tools.png' ),
+                ( 'A6D7858F-6B75-4C75-8A3D-C0B373828558', N'Офісні товари',   N'Декоративні товари для офісного облаштування', N'office.jpg' ),
+                ( 'DEF24080-00AA-440A-9690-3C9267243C43', N'Вироби зі скла',  N'Творчі вироби зі скла', N'glass.jpg' ),
+                ( '2F9A22BC-43F4-4F73-BAB1-9801052D85A9', N'Вироби з дерева', N'Композиції та декоративні твори з деревини', N'wood.jpg' ),
+                ( 'D6D9783F-2182-469A-BD08-A24068BC2A23', N'Вироби з каменя', N'Корисні та декоративні вироби з натурального каменю', N'stone.jpg' )";
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Table data inserted");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Insert Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void GroupCount_Click(object sender, RoutedEventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = @"SELECT COUNT(*) FROM ProductGroups";
+            try
+            {
+                int cnt = Convert.ToInt32(command.ExecuteScalar());
+                MessageBox.Show($"Table has {cnt} rows");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Selection Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void loadGroups() 
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "SELECT * FROM ProductGroups";
+            try
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    columns.Add($"Id {reader.GetGuid(0).ToString().Substring(0,4)}, Name : {reader.GetString(1)},\n Description: {reader.GetString(2)},\n Picture: {reader.GetString(3)}");
+                }
+                // reader.GetGuid(0);
+                // reader.GetGuid("Id"); 
+                reader.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Selection Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
